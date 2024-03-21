96764e399096b3b0d69a39c0140da2f8402a6a44
MacBook Pro <naym100m@gmail.com>
2024-03-21 11:47:02 +0600

    user crud compleate

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
-
-//resolutePointServer
-//uJpZMWNfEGKvsnxs
 
 // Middle ware
 app.use(
@@ -17,8 +14,11 @@ app.use(
 );
 app.use(express.json());
 
+//resolutePointServer
+//uJpZMWNfEGKvsnxs
 
-const uri = "mongodb+srv://resolutePointServer:uJpZMWNfEGKvsnxs@firstpractice.poejscf.mongodb.net/?retryWrites=true&w=majority&appName=FirstPractice";
+const uri =
+  "mongodb+srv://resolutePointServer:uJpZMWNfEGKvsnxs@firstpractice.poejscf.mongodb.net/?retryWrites=true&w=majority&appName=FirstPractice";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,7 +26,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -34,15 +34,31 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+
+    const usersCollections = client.db("ResoluteP").collection("users");
+
+    /// User Collection Route
+    app.get("/users", async (req, res) => {
+      const result = await usersCollections.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollections.insertOne(user);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB! 🌱");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB! 🌱"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
 
 app.get("/", (req, res) => {
   res.send("Hello Resolute Point Server! ✌️ ");
